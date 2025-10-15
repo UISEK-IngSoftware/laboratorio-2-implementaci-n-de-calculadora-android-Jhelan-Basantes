8b322d474a62b1eef1be85efc4105e57cee244ad
laboratorio.fs <jhelan.basantes@uisek.edu.ec>
2025-10-15 10:46:21 -0500

    Finalización Laboratorio 2

--- app/src/main/java/ec/edu/uisek/calculator/CalculatorScreen.kt
+++ app/src/main/java/ec/edu/uisek/calculator/CalculatorScreen.kt
@@ -1,43 +1,37 @@
 package ec.edu.uisek.calculator
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import ec.edu.uisek.calculator.ui.theme.Purple40
-import ec.edu.uisek.calculator.ui.theme.Purple80
+import ec.edu.uisek.calculator.ui.theme.Red
 import ec.edu.uisek.calculator.ui.theme.UiSekBlue
 
 @Composable
-fun CalculatorScreen() {
-    var inputText by remember { mutableStateOf("") }
+fun CalculatorScreen(viewModel: CalculatorViewModel = viewModel()) {
+
+    val state = viewModel.state
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -45,16 +39,18 @@ fun CalculatorScreen() {
             .padding(16.dp),
         verticalArrangement = Arrangement.Center
     ) {
+        // Pantalla de la calculadora (solo lectura)
         TextField(
-            value = inputText,
-            onValueChange = { inputText = it },
+            value = state.display,
+            onValueChange = { }, // no editable
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(bottom = 16.dp),
             textStyle = LocalTextStyle.current.copy(
-                fontSize = 36.sp,
+                fontSize = 56.sp,
                 textAlign = TextAlign.End,
                 color = Color.White
+
             ),
             colors = TextFieldDefaults.colors(
                 focusedContainerColor = Color.Transparent,
@@ -66,54 +62,67 @@ fun CalculatorScreen() {
             singleLine = true
         )
 
-        // Aquí colocaremos la cuadrícula de botones
-        CalculatorGrid { label ->
-            inputText += label
-        }
+        // Cuadrícula de botones
+        CalculatorGrid(onEvent = viewModel::onEvent)
     }
 }
 
 @Composable
-fun CalculatorGrid(onButtonClick: (String) -> Unit) {
+fun CalculatorGrid(onEvent: (CalculatorEvent) -> Unit) {
+
     val buttons = listOf(
         "7", "8", "9", "÷",
-        "4", "5", "6", "×",
-        "1", "2", "3", "−",
+        "4", "5", "6", "x",
+        "1", "2", "3", "-",
         "0", ".", "=", "+"
     )
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(4),
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp),
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
+
+    LazyVerticalGrid(columns = GridCells.Fixed(4)) {
         items(buttons.size) { index ->
             val label = buttons[index]
             CalculatorButton(label = label) {
-                onButtonClick(label)
+                when (label) {
+                    in "0".."9" -> onEvent(CalculatorEvent.Number(label))
+                    "." -> onEvent(CalculatorEvent.Decimal)
+                    "=" -> onEvent(CalculatorEvent.Calculate)
+                    else -> onEvent(CalculatorEvent.Operator(label))
+                }
+            }
+        }
+
+        // Botones especiales
+        item(span = { GridItemSpan(2) }) {
+            CalculatorButton(label = "AC") {
+                onEvent(CalculatorEvent.AllClear)
+            }
+        }
+        item(){}
+        item() {
+            CalculatorButton(label = "C") {
+                onEvent(CalculatorEvent.Clear)
             }
         }
     }
 }
 
-
 @Composable
 fun CalculatorButton(label: String, onClick: () -> Unit) {
-    Box (
+
+    Box(
         modifier = Modifier
-            .aspectRatio(1f)
-            .fillMaxSize()
+            .padding(4.dp)
+            .aspectRatio(ratio = if (label == "AC") 2f else 1f)
             .clip(CircleShape)
-            .background(if (label in listOf("÷", "×", "−", "+", "=", "."))
-                Purple40
-                else
-                UiSekBlue
+            .background(
+                when (label) {
+                    in listOf("÷", "x", "-", "+", "=", ".") -> Purple40
+                    in listOf("AC","C") -> Red
+                    else -> UiSekBlue
+                }
             )
             .clickable { onClick() },
         contentAlignment = Alignment.Center
-    ){
+    ) {
         Text(
             text = label,
             color = Color.White,
